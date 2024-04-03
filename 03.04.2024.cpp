11cd43001a3915e2e6826a982f97aae686ab041e
Ram Mundra <115203473+2004ram@users.noreply.github.com>
2024-04-03 21:39:54 +0530

    Update 03.04.2024.cpp

--- 03.04.2024.cpp
+++ 03.04.2024.cpp
@@ -1,1 +1,15 @@
 // 03.04.2024 gfg potd solution
+class Solution {
+    public int kthCommonAncestor(Node root, int k, int x, int y) {
+        if (x == y) return -1;
+        List<Integer> list = new ArrayList<>();
+        while (root != null){
+            list.add(root.data);
+            if (x < root.data && y < root.data) root = root.left;
+            else if (x > root.data && y > root.data) root = root.right;
+            else break;
+        }
+        if (k > list.size()) return -1;
+        return list.get(list.size() - k);
+    }
+}
